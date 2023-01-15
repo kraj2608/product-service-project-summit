74b017c677d756f4d4a16925028e78f01d181b6d
Kaveesha Rajapaksha <kraj2608@sysco.com>
2023-01-15 18:27:20 +0530

    Product getting api added

---
+++ src/main/java/com/foodshop/productservice/constants/ErrorMessages.java
@@ -0,0 +1,7 @@
+package com.foodshop.productservice.constants;
+
+public class ErrorMessages {
+
+    private ErrorMessages(){}
+    public static final String PRODUCT_NOT_FOUND = "Product Not Found";
+}
